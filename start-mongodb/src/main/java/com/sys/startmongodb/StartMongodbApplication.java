1d862ee29971924a4809716c026013b6cd0fd552
catalpa65 <59327204+catalpa65@users.noreply.github.com>
2020-07-01 16:18:27 +0800

    初始化提交

---
+++ start-mongodb/src/main/java/com/sys/startmongodb/StartMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.sys.startmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartMongodbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartMongodbApplication.class, args);
+    }
+
+}
---
+++ start-mongodb/src/main/java/com/sys/startmongodb/Test.java
@@ -0,0 +1,13 @@
+package com.sys.startmongodb;
+
+/**
+ * Test
+ *
+ * @author yangjingsong
+ * @since 2020/7/1
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
